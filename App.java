7c1ac92c76f0e42ee63459d08825989074aa897f
Jason Williams <jason.williams@capaciti.org.za>
2025-12-11 13:13:52 +0200

    Initial commit

--- App.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-
-public class App extends JFrame {
-    
-    public App() {
-        this.setVisible(true);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
-}
